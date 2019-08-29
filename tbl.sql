a9ed2b274b299d64ccc4496e6b98e2ef168ce249
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-08-29 11:48:02 +0800

    update

--- tbl.sql
+++ tbl.sql
@@ -1,6 +1,12 @@
 -- 查看所有表和视图，以及注释
-SELECT tc.TABLE_NAME || ' ' || tc.COMMENTS
+SET HEADING OFF;
+
+SELECT DECODE(tc.TABLE_TYPE, 'TABLE', 'T', 'VIEW', 'V', 'X')
+         || ' ' || tc.TABLE_NAME || ' ' ||
+         REPLACE(REPLACE(TC.COMMENTS, CHR(13), ''), CHR(10), ' _R_N ')
   FROM USER_TAB_COMMENTS tc
  WHERE tc.TABLE_TYPE in ('TABLE', 'VIEW') AND
        REGEXP_LIKE(tc.TABLE_NAME, '^[0-9A-Za-z][_0-9A-Za-z]*$')
  ORDER BY tc.TABLE_NAME;
+
+SET HEADING ON;
