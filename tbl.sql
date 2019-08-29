4e05ec6624d1486ed72f2541119b8d091e4e9b6f
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-08-29 11:40:08 +0800

    fix

--- tbl.sql
+++ tbl.sql
@@ -1,5 +1,7 @@
 -- 查看所有表和视图，以及注释
 SELECT tc.TABLE_NAME || ' ' || tc.COMMENTS
   FROM USER_TAB_COMMENTS tc
-   WHERE tc.TABLE_TYPE in ('TABLE', 'VIEW')
+   WHERE tc.TABLE_TYPE in ('TABLE', 'VIEW') 
+     AND
+     REGEXP_LIKE(tc.TABLE_NAME, '^[0-9A-Za-z][_0-9A-Za-z]*$')
    ORDER BY tc.TABLE_NAME;
