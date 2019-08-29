45431ef91c50db10ee19c02c6c8ef8acdba6112c
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-08-29 11:41:04 +0800

    disable system name

--- tbl.sql
+++ tbl.sql
@@ -1,7 +1,6 @@
 -- 查看所有表和视图，以及注释
 SELECT tc.TABLE_NAME || ' ' || tc.COMMENTS
   FROM USER_TAB_COMMENTS tc
-   WHERE tc.TABLE_TYPE in ('TABLE', 'VIEW') 
-     AND
-     REGEXP_LIKE(tc.TABLE_NAME, '^[0-9A-Za-z][_0-9A-Za-z]*$')
-   ORDER BY tc.TABLE_NAME;
+ WHERE tc.TABLE_TYPE in ('TABLE', 'VIEW') AND
+       REGEXP_LIKE(tc.TABLE_NAME, '^[0-9A-Za-z][_0-9A-Za-z]*$')
+ ORDER BY tc.TABLE_NAME;
