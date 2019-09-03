39ed7080d9d6cc070e147c35b9f1264bfecd8589
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-03 18:21:38 +0800

    add darwin arch

--- termwidth.sh
+++ termwidth.sh
@@ -1,9 +1,11 @@
 _termwidth() {
-  local osname=$(uname -o)
+  local osname=$(uname -a | cut -d' ' -f1)
 
   case "${osname}" in
-    "GNU/Linux" )
+    "Linux" )
       stty -a | head -n 1 | cut -d';' -f3 | cut -d' ' -f3 ;;
+    "Darwin" )
+      stty -a | head -n 1 | cut -d';' -f3 | cut -d' ' -f2 ;;
     * )
       echo 80 ;;
   esac
