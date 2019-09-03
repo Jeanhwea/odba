ad3d93608a133137e48c239411d7d9d02681eb5b
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-03 16:45:51 +0800

    compress column name

--- t.sql
+++ t.sql
@@ -8,23 +8,23 @@ DEFINE tablename = &1;
 -- TTITLE CENTER 'Columns of Table [&tablename]' SKIP 1 LINE;
 -- COLUMN tabname FORMAT A32 HEADING 'Table';
 COLUMN colname FORMAT A32 HEADING 'Name';
-COLUMN coltype FORMAT A16 HEADING 'Type';
-COLUMN collen FORMAT 999999 HEADING 'Length';
-COLUMN colnull FORMAT A6 HEADING 'Null?';
-COLUMN ispk FORMAT A4 HEADING 'PK?';
+COLUMN coltype FORMAT A10 HEADING 'Type';
+COLUMN collen FORMAT 9999 HEADING 'Len';
+COLUMN colnull FORMAT A2 HEADING 'NN';
+COLUMN ispk FORMAT A2 HEADING 'PK';
 COLUMN colcmt FORMAT A80 HEADING 'Comment' TRUNCATE;
 
 
 SELECT tc.COLUMN_NAME AS colname,
        tc.DATA_TYPE AS coltype,
        tc.DATA_LENGTH AS collen,
-       DECODE(tc.NULLABLE, 'N', 'No', 'Y', 'Yes') AS colnull,
+       DECODE(tc.NULLABLE, 'N', '*', '-') AS colnull,
        DECODE((SELECT cs.CONSTRAINT_TYPE
                  FROM USER_CONS_COLUMNS uc, USER_CONSTRAINTS cs
                 WHERE cs.CONSTRAINT_NAME = uc.CONSTRAINT_NAME AND
                       tc.TABLE_NAME = uc.TABLE_NAME AND
                       tc.COLUMN_NAME = uc.COLUMN_NAME AND
-                      cs.CONSTRAINT_TYPE = 'P'), 'P', 'Yes', '') AS ispk,
+                      cs.CONSTRAINT_TYPE = 'P'), 'P', '*', '-') AS ispk,
        REPLACE(REPLACE(cc.COMMENTS, CHR(13), ''), CHR(10), ' _R_N ') AS colcmt
   FROM USER_TAB_COLUMNS tc
          LEFT JOIN USER_COL_COMMENTS cc
