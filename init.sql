d0d0d17cf07171e26cc2795c8767e4da478ebad5
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-03 15:48:29 +0800

    update

--- init.sql
+++ init.sql
@@ -1,5 +1,14 @@
 -- setup line
+SET TERMOUT OFF;
+
+SET PAGESIZE 50000;
+
+-- setup line width to terminal screen width
 HOST echo "SET LINESIZE $(./termwidth.sh)" > .tmp.sql;
 @.tmp.sql;
 HOST rm .tmp.sql;
+
+-- change data format to common readable format
 ALTER SESSION SET nls_date_format = 'yyyy-mm-dd hh24:mi:ss';
+
+SET TERMOUT ON;
---
+++ t.sql
@@ -0,0 +1,27 @@
+SET LINESIZE 32767;
+SET PAGESIZE 50000;
+SET FEEDBACK OFF;
+SET TAB OFF;
+
+DEFINE tablename = &1;
+
+-- TTITLE CENTER 'Columns of Table [&tablename]' SKIP 1 LINE;
+-- COLUMN tabname FORMAT A32 HEADING 'Table';
+COLUMN colname FORMAT A32 HEADING 'Name';
+COLUMN coltype FORMAT A16 HEADING 'Type';
+COLUMN collen FORMAT 999999 HEADING 'Length';
+COLUMN colnull FORMAT A8 HEADING 'Nullable';
+COLUMN colcmt FORMAT A80 HEADING 'Comment' TRUNCATE;
+
+
+SELECT tc.COLUMN_NAME AS colname,
+       tc.DATA_TYPE AS coltype,
+       tc.DATA_LENGTH AS collen,
+       DECODE(tc.NULLABLE, 'N', 'Not Null', 'Y', '') AS colnull,
+       REPLACE(REPLACE(cc.COMMENTS, CHR(13), ''), CHR(10), ' _R_N ') AS colcmt
+  FROM USER_TAB_COLUMNS tc
+         LEFT JOIN USER_COL_COMMENTS cc
+             ON tc.TABLE_NAME = cc.TABLE_NAME AND
+         tc.COLUMN_NAME = cc.COLUMN_NAME
+ WHERE UPPER(tc.TABLE_NAME) = UPPER('&tablename')
+ ORDER BY tc.COLUMN_ID;
