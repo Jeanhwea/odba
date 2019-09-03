40658e070d2607be45d475f1278a1166987bdcd7
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-03 15:55:32 +0800

    update

--- termwidth.sh
+++ termwidth.sh
@@ -1,1 +1,13 @@
-stty -a | head -n 1 | cut -d';' -f3 | cut -d' ' -f3
+_termwidth() {
+  local osname=$(uname -o)
+
+  case "${osname}" in
+    "GNU/Linux" )
+      stty -a | head -n 1 | cut -d';' -f3 | cut -d' ' -f3 ;;
+    * )
+      echo 80 ;;
+  esac
+
+}
+
+_termwidth
