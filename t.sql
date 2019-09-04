0904d47d53cd88b7a09f7f0f874b87201309e6d2
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-04 13:07:08 +0800

    add

--- t.sql
+++ t.sql
@@ -19,13 +19,13 @@ COLUMN colcmt FORMAT A80 HEADING 'Comment' TRUNCATE;
 SELECT tc.COLUMN_NAME AS colname,
        tc.DATA_TYPE AS coltype,
        tc.DATA_LENGTH AS collen,
-       DECODE(tc.NULLABLE, 'N', '!', '-') AS colnull,
+       DECODE(tc.NULLABLE, 'N', '!', '') AS colnull,
        DECODE((SELECT cs.CONSTRAINT_TYPE
                  FROM USER_CONS_COLUMNS uc, USER_CONSTRAINTS cs
                 WHERE cs.CONSTRAINT_NAME = uc.CONSTRAINT_NAME AND
                       tc.TABLE_NAME = uc.TABLE_NAME AND
                       tc.COLUMN_NAME = uc.COLUMN_NAME AND
-                      cs.CONSTRAINT_TYPE = 'P'), 'P', '*', '-') AS ispk,
+                      cs.CONSTRAINT_TYPE = 'P'), 'P', '*', '') AS ispk,
        REPLACE(REPLACE(cc.COMMENTS, CHR(13), ''), CHR(10), ' _R_N ') AS colcmt
   FROM USER_TAB_COLUMNS tc
          LEFT JOIN USER_COL_COMMENTS cc
