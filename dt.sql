6868341914e2509c2b10d0050ae75b747c6c21fb
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-09 19:16:23 +0800

    update some column

--- dt.sql
+++ dt.sql
@@ -4,15 +4,15 @@ SET FEEDBACK OFF;
 SET TAB OFF;
 
 TTITLE LEFT 'List of User Tables' SKIP 1 LINE;
-COLUMN tsname FORMAT A20 HEADING 'Tablespace';
-COLUMN tabname FORMAT A32 HEADING 'Tablename';
+-- COLUMN tsname FORMAT A20 HEADING 'Tablespace';
+COLUMN tabname FORMAT A32 HEADING 'Table Name';
 COLUMN tabcmt FORMAT A80 HEADING 'Comments' TRUNCATE;
 
 SELECT
-  utbs.TABLESPACE_NAME AS tsname,
+  -- utbs.TABLESPACE_NAME AS tsname,
   utbs.TABLE_NAME AS tabname,
   REPLACE(REPLACE(utbcmts.COMMENTS, CHR(13), ''), CHR(10), '\n') AS tabcmt
   FROM USER_TABLES utbs
          LEFT JOIN USER_TAB_COMMENTS utbcmts
              ON utbs.TABLE_NAME = utbcmts.TABLE_NAME
- ORDER BY tsname, tabname;
+ ORDER BY tabname;
