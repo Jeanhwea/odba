ab733e2e539718f19ad84e9c96900739e9c854b4
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-10 09:00:56 +0800

    add export bamtri

---
+++ bamtri/exp109.bat
@@ -0,0 +1,1 @@
+exp bamtri_mes/bamtri_mes@192.168.0.214/ora10g PARFILE=options.txt LOG=exp109.log OWNER=bamtri_mes
---
+++ bamtri/imp240.bat
@@ -0,0 +1,1 @@
+imp bamtri_mes/bamtri_mes@192.168.0.240/xe PARFILE=options.txt LOG=imp240.log FROMUSER=bamtri_mes TOUSER=bamtri_mes
