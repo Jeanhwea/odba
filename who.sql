98dddcbceed66e025d8b0ffdf61e3cd9ecc22125
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-10 10:02:10 +0800

    add more

--- who.sql
+++ who.sql
@@ -4,17 +4,23 @@ SET FEEDBACK OFF;
 SET TAB OFF;
 
 TTITLE LEFT 'List of User Sessions' SKIP 1 LINE;
+COLUMN logintype FORMAT A16 HEADING 'Type';
+COLUMN logontime HEADING 'Login Time';
 COLUMN username FORMAT A16 HEADING 'User Name';
 COLUMN servid FORMAT A16 HEADING 'Service Name';
-COLUMN machine FORMAT A24 HEADING 'MACHINE';
+COLUMN osid FORMAT A24 HEADING 'User@Hostname';
+COLUMN program FORMAT A24 HEADING 'Program';
 COLUMN status FORMAT A10 HEADING 'Status';
 COLUMN state FORMAT A20 HEADING 'State';
 
 SELECT
+  sess.TYPE AS logintype,
+  sess.LOGON_TIME AS logontime,
   sess.USERNAME AS username,
   sess.SERVICE_NAME AS servid,
-  sess.MACHINE AS machine,
+  sess.OSUSER || '@' || sess.MACHINE AS osid,
+  sess.PROGRAM AS program,
   sess.STATUS AS status,
   sess.STATE AS state
   FROM V$SESSION sess
- ORDER BY username, status;
+ ORDER BY logintype, username, status;
