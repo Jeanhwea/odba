2ca5cf86468bc64a5af2bc87e4f8192ed5e2fcf2
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-17 10:01:48 +0800

    update exp data commands

--- bamtri/expdata.sh
+++ bamtri/expdata.sh
@@ -3,12 +3,14 @@ export ORACLE_HOME=/u01/app/oracle/product/11.2.0/xe
 export PATH=$ORACLE_HOME/bin:$PATH
 export ORACLE_SID=XE
 export NLS_LANG=".AL32UTF8"
+export TZ='Asia/Shanghai'
 
 HERE=`cd $(dirname $0); pwd`
 TAG=$(date +'%Y%m%d')
 LOGFILE=exp231.log
+DATFILE=data.dmp
 
 cd $HERE &&
-exp bamtri_mes/bamtri_mes@localhost/xe PARFILE=options.txt LOG=${LOGFILE} OWNER=bamtri_mes &&
-zip data${TAG}.zip data.dmp ${LOGFILE} &&
+exp PARFILE=expopts.txt LOG=${LOGFILE} FILE=${DATFILE} &&
+zip data${TAG}.zip ${DATFILE} ${LOGFILE} &&
 rm data.dmp ${LOGFILE}
