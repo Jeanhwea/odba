46aed9e89f25aad536774f5f30580b180d1ffd9f
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-18 16:07:06 +0800

    fix bug

--- bamtri/upload.sh
+++ bamtri/upload.sh
@@ -2,4 +2,4 @@ DATFILE=$(find $(pwd) -type f -iname 'data*.zip' | sort -r | head -n 1)
 SERVER=192.168.0.240
 
 echo "upload ${DATFILE} to ${SERVER}"
-scp $DATFILE ub64@${SERVER}:tmp
+scp $DATFILE ub64@${SERVER}:/tmp
