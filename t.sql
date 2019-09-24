14723d50f11dfee8b1a523c7ecc1077824f50178
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-09-24 11:04:57 +0800

    更新显示格式

--- t.sql
+++ t.sql
@@ -4,35 +4,56 @@ SET FEEDBACK OFF;
 SET TAB OFF;
 SET VERIFY OFF;
 SET TERMOUT OFF;
-
 DEFINE tablename = '&1';
+SET TERMOUT ON;
 
-TTITLE LEFT 'List columns of table [&tablename]' SKIP 1 LINE;
+
 COLUMN colname FORMAT A25 HEADING 'Name';
 COLUMN coltype FORMAT A10 HEADING 'Type';
 COLUMN collen FORMAT 9999 HEADING 'Len';
 COLUMN colpcs FORMAT 9999 HEADING 'Prec';
 COLUMN colnull FORMAT A2 HEADING 'NN';
 COLUMN ispk FORMAT A2 HEADING 'PK';
+COLUMN isuniq FORMAT A2 HEADING 'UQ';
 COLUMN colcmt FORMAT A80 HEADING 'Comment' TRUNCATE;
+COLUMN cstname FORMAT A32 HEADING 'Constraint';
 
-SET TERMOUT ON;
+
+-- list constraint
+TTITLE LEFT 'List constraints of table [&tablename]' SKIP 1 LINE;
+SELECT
+  uccl.CONSTRAINT_NAME AS cstname,
+  uccl.COLUMN_NAME AS colname
+  FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
+ WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
+       ucst.CONSTRAINT_TYPE = 'U' AND
+       UPPER(uccl.TABLE_NAME) = UPPER('&tablename')
+ ORDER BY uccl.POSITION;
 
 -- Maximun length of user column name
 -- SELECT MAX(LENGTH(COLUMN_NAME)) USER_TAB_COLUMNS;
 
 -- list table columns
+TTITLE LEFT 'List columns of table [&tablename]' SKIP 1 LINE;
 SELECT
-  DECODE(
-    (SELECT ucst.CONSTRAINT_TYPE
-       FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
-      WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
-            utbc.COLUMN_NAME = uccl.COLUMN_NAME AND
-            utbc.TABLE_NAME = uccl.TABLE_NAME AND
-            ucst.CONSTRAINT_TYPE = 'P' AND
-            ROWNUM <= 1), 'P', '*', '')
+  (SELECT 'P'
+     FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
+    WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
+          utbc.COLUMN_NAME = uccl.COLUMN_NAME AND
+          utbc.TABLE_NAME = uccl.TABLE_NAME AND
+          ucst.CONSTRAINT_TYPE = 'P' AND
+          ROWNUM <= 1)
     AS ispk,
-  DECODE(utbc.NULLABLE, 'N', '!', '') AS colnull,
+  (SELECT 'U'
+     FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
+    WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
+          utbc.COLUMN_NAME = uccl.COLUMN_NAME AND
+          utbc.TABLE_NAME = uccl.TABLE_NAME AND
+          ucst.CONSTRAINT_TYPE = 'U'
+    GROUP BY ucst.CONSTRAINT_NAME
+   HAVING COUNT(1) = 1)
+    AS isuniq,
+  DECODE(utbc.NULLABLE, 'N', '*', '') AS colnull,
   utbc.COLUMN_NAME AS colname,
   utbc.DATA_TYPE AS coltype,
   utbc.DATA_LENGTH AS collen,
@@ -47,18 +68,3 @@ SELECT
   FROM USER_TAB_COLUMNS utbc
  WHERE UPPER(utbc.TABLE_NAME) = UPPER('&tablename')
  ORDER BY ispk, colnull, colname;
-
-
-TTITLE LEFT 'List constraints of table [&tablename]' SKIP 1 LINE;
-COLUMN cstname FORMAT A32 HEADING 'Constraint';
-COLUMN colname FORMAT A32 HEADING 'Name';
-
--- list constraint
-SELECT
-  uccl.CONSTRAINT_NAME AS cstname,
-  uccl.COLUMN_NAME AS colname
-  FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
- WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
-       ucst.CONSTRAINT_TYPE = 'U' AND
-       UPPER(uccl.TABLE_NAME) = UPPER('&tablename')
- ORDER BY uccl.POSITION;
