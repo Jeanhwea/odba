195bf27c6f6c4218d813c29ce8052ac97695ed35
Jinghui Hu <hujinghui@buaa.edu.cn>
2019-11-13 13:08:00 +0800

    添加导出 213 数据库的脚本

--- bamtri/exp213.bat
+++ bamtri/exp213.bat
@@ -1,9 +1,9 @@
 @echo off
 set tag=%date:~0,4%%date:~5,2%%date:~8,2%
-set logfile=exp214.log
+set logfile=exp213.log
 set datfile=data.dmp
 
-exp PARFILE=expopts214.txt LOG=%logfile% FILE=%datfile%
+exp PARFILE=expopts213.txt LOG=%logfile% FILE=%datfile%
 zip data%tag%.zip %datfile% %logfile%
 rm %datfile% %logfile%
 mv data%tag%.zip dump
