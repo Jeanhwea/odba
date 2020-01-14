7511505b2fcdef7d51018fa3f95fc2581534177f
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-01-14 10:58:19 +0800

    修改导出 213 脚本

--- bamtri/exp213.bat
+++ bamtri/exp213.bat
@@ -1,9 +1,16 @@
 @echo off
-set tag=%date:~0,4%%date:~5,2%%date:~8,2%
-set logfile=exp213.log
-set datfile=data.dmp
+set datetag=%date:~0,4%%date:~5,2%%date:~8,2%
+set timetag=%time:~0,2%%time:~3,2%%time:~6,2%
+set filetag=%datetag%_%timetag%
 
-exp PARFILE=expopts213.txt LOG=%logfile% FILE=%datfile%
-zip data%tag%.zip %datfile% %logfile%
-rm %datfile% %logfile%
-mv data%tag%.zip dump
+set logfile=%filetag%_export.log
+set datfile=%filetag%_export.dmp
+set server=192.168.0.213
+set userid=bamtri_mes/bamtri_mes@%server%/ora10g
+
+set NLS_LANG='.ZHS16GBK'
+exp PARFILE=expopts213.txt USERID=%userid% LOG=%logfile% FILE=%datfile%
+
+rem zip data%tag%.zip %datfile% %logfile%
+rem rm %datfile% %logfile%
+rem mv data%tag%.zip dump
