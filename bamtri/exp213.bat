5c18b7ceef50d341daff178646ac43cc36125d21
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-01-14 11:03:55 +0800

    完成 NLS_LANG 环境变量的修改

--- bamtri/exp213.bat
+++ bamtri/exp213.bat
@@ -1,4 +1,6 @@
 @echo off
+set NLS_LANG=.ZHS16GBK
+
 set datetag=%date:~0,4%%date:~5,2%%date:~8,2%
 set timetag=%time:~0,2%%time:~3,2%%time:~6,2%
 set filetag=%datetag%_%timetag%
@@ -8,8 +10,9 @@ set datfile=%filetag%_export.dmp
 set server=192.168.0.213
 set userid=bamtri_mes/bamtri_mes@%server%/ora10g
 
-set NLS_LANG='.ZHS16GBK'
+echo Export from %userid%
 exp PARFILE=expopts213.txt USERID=%userid% LOG=%logfile% FILE=%datfile%
+echo Save log to %logfile%
 
 rem zip data%tag%.zip %datfile% %logfile%
 rem rm %datfile% %logfile%
