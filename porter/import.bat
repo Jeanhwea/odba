b8d6a10ea322272f90fb3ce2228b6c0df0b2fbb3
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-01-15 09:04:16 +0800

    修复获取时间最后百分号漏写的问题

--- porter/import.bat
+++ porter/import.bat
@@ -12,10 +12,10 @@ set NLS_LANG=.ZHS16GBK
 
 rem 处理日期和是时间
 set datetag=%date:~0,4%%date:~5,2%%date:~8,2%
-set timetag=%time:~0,2%%time:~3,2%%time:~6,2
+set timetag=%time:~0,2%%time:~3,2%%time:~6,2%
 rem 如果小时数小于 10，需要手工将前面补 0
 set hour=%time:~0,2%
-if /i %hour% LSS 10 (set timetag=0%time:~1,1%%time:~3,2%%time:~6,2)
+if /i %hour% LSS 10 (set timetag=0%time:~1,1%%time:~3,2%%time:~6,2%)
 set filetag=%datetag%_%timetag%
 
 rem 处理文件夹和文件
