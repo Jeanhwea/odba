5b48f15dbfd46462a8c95461b1b0a62d0f8b4a67
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-09 22:06:47 +0800

    fix

--- da.sql
+++ da.sql
@@ -1,18 +1,16 @@
 @preaction;
 ttitle left 'List of User Tables' skip 1 line;
 column tablespace_name format a20 heading 'Tablespace';
-column table_name format a32 heading 'Table';
-column table_status format a16 heading 'Status';
-column table_comments format a80 heading 'Comments' truncate;
+column table_name      format a32 heading 'Table';
+column table_status    format a16 heading 'Status';
+column table_comments  format a80 heading 'Comments' truncate;
 
 select
-  a.tablespace_name as tablespace_name,
-  a.table_name as table_name,
-  -- a.status as table_status,
-  replace(replace(b.comments, chr(13), ''), chr(10), '\n') as table_comments
+  t1.tablespace_name as tablespace_name,
+  t1.table_name as table_name,
+  -- t1.status as table_status,
+  replace(replace(t2.comments, chr(13), ''), chr(10), '\n') as table_comments
 from
-  all_tables a
-  left join user_tab_comments b on a.table_name = b.table_name
-order by
-  tablespace_name,
-  table_name;
+  all_tables t1
+  left join user_tab_comments t2 on t1.table_name = t2.table_name
+order by tablespace_name, table_name;
