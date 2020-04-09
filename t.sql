094f764d4abc0e9d7fbac2bab123e281e01d686a
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-09 21:54:30 +0800

    lowercase

--- t.sql
+++ t.sql
@@ -1,70 +1,68 @@
-SET LINESIZE 255;
-SET PAGESIZE 50000;
-SET FEEDBACK OFF;
-SET TAB OFF;
-SET VERIFY OFF;
-SET TERMOUT OFF;
-DEFINE tablename = '&1';
-SET TERMOUT ON;
+set linesize 255;
+set pagesize 50000;
+set feedback off;
+set tab off;
+set verify off;
+set termout off;
+define tablename = '&1';
+set termout on;
 
-
-COLUMN colname FORMAT A25 HEADING 'Name';
-COLUMN coltype FORMAT A10 HEADING 'Type';
-COLUMN collen FORMAT 9999 HEADING 'Len';
-COLUMN colpcs FORMAT 9999 HEADING 'Prec';
-COLUMN colnull FORMAT A2 HEADING 'NN';
-COLUMN ispk FORMAT A2 HEADING 'PK';
-COLUMN isuniq FORMAT A2 HEADING 'UQ';
-COLUMN colcmt FORMAT A80 HEADING 'Comment' TRUNCATE;
-COLUMN cstname FORMAT A32 HEADING 'Constraint';
-
+column colname format a25  heading 'Name';
+column coltype format a10  heading 'Type';
+column collen  format 9999 heading 'Len';
+column colpcs  format 9999 heading 'Prec';
+column colnull format a2   heading 'NN';
+column ispk    format a2   heading 'PK';
+column isuniq  format a2   heading 'UQ';
+column colcmt  format a80  heading 'Comment' truncate;
+column cstname format a32  heading 'Constraint';
 
 -- list constraint
-TTITLE LEFT 'List constraints of table [&tablename]' SKIP 1 LINE;
-SELECT
-  uccl.CONSTRAINT_NAME AS cstname,
-  uccl.COLUMN_NAME AS colname
-  FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
- WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
-       ucst.CONSTRAINT_TYPE = 'U' AND
-       UPPER(uccl.TABLE_NAME) = UPPER('&tablename')
- ORDER BY uccl.POSITION;
+ttitle left 'List constraints of table [&tablename]' skip 1 line;
+select
+  t1.constraint_name as cstname,
+  t1.column_name as colname
+  from user_cons_columns t1, user_constraints t2
+ where t2.constraint_name = t1.constraint_name and
+       lower(t2.constraint_type) = 'u' and
+       lower(t1.table_name) = lower('&tablename')
+ order by t1.position;
 
--- Maximun length of user column name
--- SELECT MAX(LENGTH(COLUMN_NAME)) USER_TAB_COLUMNS;
+-- maximum length of user column name
+-- select max(length(column_name)) user_tab_columns;
 
 -- list table columns
-TTITLE LEFT 'List columns of table [&tablename]' SKIP 1 LINE;
-SELECT
-  (SELECT 'P'
-     FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
-    WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
-          utbc.COLUMN_NAME = uccl.COLUMN_NAME AND
-          utbc.TABLE_NAME = uccl.TABLE_NAME AND
-          ucst.CONSTRAINT_TYPE = 'P' AND
-          ROWNUM <= 1)
-    AS ispk,
-  (SELECT 'U'
-     FROM USER_CONS_COLUMNS uccl, USER_CONSTRAINTS ucst
-    WHERE ucst.CONSTRAINT_NAME = uccl.CONSTRAINT_NAME AND
-          utbc.COLUMN_NAME = uccl.COLUMN_NAME AND
-          utbc.TABLE_NAME = uccl.TABLE_NAME AND
-          ucst.CONSTRAINT_TYPE = 'U'
-    GROUP BY ucst.CONSTRAINT_NAME
-   HAVING COUNT(1) = 1)
-    AS isuniq,
-  DECODE(utbc.NULLABLE, 'N', '*', '') AS colnull,
-  utbc.COLUMN_NAME AS colname,
-  utbc.DATA_TYPE AS coltype,
-  utbc.DATA_LENGTH AS collen,
-  utbc.DATA_PRECISION AS colpcs,
+ttitle left 'List columns of table [&tablename]' skip 1 line;
+select
+  (select 'p'
+     from user_cons_columns t2, user_constraints t3
+    where t3.constraint_name = t2.constraint_name and
+          t1.column_name = t2.column_name and
+          t1.table_name = t2.table_name and
+          lower(t3.constraint_type) = 'p' and
+          rownum <= 1)
+    as ispk,
+  (select 'u'
+     from user_cons_columns t4, user_constraints t5
+    where t5.constraint_name = t4.constraint_name and
+          t1.column_name = t4.column_name and
+          t1.table_name = t4.table_name and
+          lower(t5.constraint_type) = 'u'
+    group by t5.constraint_name
+   having count(1) = 1)
+    as isuniq,
+  decode(lower(t1.nullable), 'n', '*', '') as colnull,
+  t1.column_name as colname,
+  t1.data_type as coltype,
+  t1.data_length as collen,
+  t1.data_precision as colpcs,
   (
-    SELECT REPLACE(REPLACE(uclc.COMMENTS, CHR(13), ''), CHR(10), '\n')
-      FROM USER_COL_COMMENTS uclc
-     WHERE uclc.COLUMN_NAME = utbc.COLUMN_NAME AND
-           uclc.TABLE_NAME = utbc.TABLE_NAME AND
-           ROWNUM <= 1
-  ) AS colcmt
-  FROM USER_TAB_COLUMNS utbc
- WHERE UPPER(utbc.TABLE_NAME) = UPPER('&tablename')
- ORDER BY ispk, colnull, colname;
+    select replace(replace(t6.comments, chr(13), ''), chr(10), '\n')
+      from user_col_comments t6
+     where t6.column_name = t1.column_name and
+           t6.table_name = t1.table_name and
+           rownum <= 1
+  ) as colcmt
+  from user_tab_columns t1
+ where lower(t1.table_name) = lower('&tablename')
+ order by ispk, colnull, colname;
