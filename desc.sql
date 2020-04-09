91fe2ff9635adbf26f96009f2c6d0cfbfebf30dd
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-09 22:23:53 +0800

    update

--- desc.sql
+++ desc.sql
@@ -3,23 +3,23 @@ set verify off;
 define tablename = &1;
 
 ttitle left 'Columns of Table [&tablename]' skip 1 line;
-column column_name format a32 heading 'Name';
-column column_type format a16 heading 'Type';
-column column_length format 999999 heading 'Length';
-column column_null format a8 heading 'Nullable';
-column column_comments format a80 heading 'Comments' truncate;
+column colname format a32 heading 'Name';
+column coltype format a16 heading 'Type';
+column collen  format 999999 heading 'Length';
+column isnul   format a8 heading 'Nullable';
+column colcmt  format a80 heading 'Comments' truncate;
 
 select
-  a.column_name as column_name,
-  a.data_type as column_type,
-  a.data_length as column_length,
-  decode(a.NULLABLE, 'N', 'Not Null', 'Y', '') as column_null,
-  replace(replace(b.comments, chr(13), ''), chr(10), '_rn_') as column_comments
+  t1.column_name as colname,
+  t1.data_type as coltype,
+  t1.data_length as collen,
+  decode(t1.NULLABLE, 'N', 'Not Null', 'Y', '') as isnul,
+  replace(replace(t2.comments, chr(13), ''), chr(10), '_rn_') as colcmt
 from
-  user_tab_columns a
-  left join user_col_comments b on a.table_name = b.table_name
-    and a.column_name = b.column_name
+  user_tab_columns t1
+  left join user_col_comments t2 on t1.table_name = t2.table_name
+    and t1.column_name = t2.column_name
 where
-  upper(a.table_name) = upper('&tablename')
+  lower(t1.table_name) = lower('&tablename')
 order by
-  a.column_id;
+  t1.column_id;
