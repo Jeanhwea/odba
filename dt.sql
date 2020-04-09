af62d3d3989a1a62605efbbb150e2f7726dcf81a
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-04-09 22:32:40 +0800

    update

--- dt.sql
+++ dt.sql
@@ -1,20 +1,19 @@
 @preaction;
 ttitle left 'List of User Tables' skip 1 line;
-column tablespace_name format a20 heading 'Table Space Name';
-column table_name format a32 heading 'Table Name';
-column table_comments format a80 heading 'Comments' truncate;
+column tspname format a20 heading 'Tablespace';
+column tabname format a32 heading 'Table';
+column tabcmt  format a80 heading 'Comments' truncate;
 
 -- select max(length(table_name)) from user_tables;
 -- select max(length(comments)) from user_tab_comments;
 
 select
-  -- a.tablespace_name as tablespace_name,
-  a.table_name as table_name,
-  replace(replace(b.comments, chr(13), ''), chr(10), '_rn_') as table_comments
+  -- t1.tablespace_name as tspname,
+  t1.table_name as tabname,
+  replace(replace(t2.comments, chr(13), ''), chr(10), '_rn_') as tabcmt
 from
-  user_tables a,
-  User_tab_comments b
+  user_tables t1, user_tab_comments t2
 where
-  a.table_name = b.table_name
+  t1.table_name = t2.table_name
 order by
-  table_name;
+  tabname;
