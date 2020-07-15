d7943c3132bfa97367040f292a55bb5c9becb227
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-07-15 18:46:51 +0800

    update

--- porter/import.bat
+++ porter/import.bat
@@ -2,30 +2,30 @@
 rem ----------------------------------------------------------------------------
 rem 启动前需要修改的配置
 rem ----------------------------------------------------------------------------
-set server=192.168.0.215
-set sid=ora10g
-set user=system/dba
+set TNSID=192.168.0.231/xe
+set SYSUSER=system
+set SYSPASS=dba
 rem ----------------------------------------------------------------------------
 
 rem 环境变量
 set NLS_LANG=.ZHS16GBK
 
 rem 处理日期和时间
-set datetag=%date:~0,4%%date:~5,2%%date:~8,2%
-set timetag=%time:~0,2%%time:~3,2%%time:~6,2%
+set DATETAG=%date:~0,4%%date:~5,2%%date:~8,2%
+set TIMETAG=%time:~0,2%%time:~3,2%%time:~6,2%
 rem 如果小时数小于 10，需要手工将前面补 0
-set hour=%time:~0,2%
-if /i %hour% LSS 10 (set timetag=0%time:~1,1%%time:~3,2%%time:~6,2%)
-set filetag=%datetag%_%timetag%
+set HOUR=%time:~0,2%
+if /i %HOUR% LSS 10 (set TIMETAG=0%time:~1,1%%time:~3,2%%time:~6,2%)
+set FILETAG=%DATETAG%_%TIMETAG%
 
 rem 处理文件夹和文件
-set datadir=data
-set logfile=%datadir%\%filetag%_import.log
-set datfile=%datadir%\data.dmp
-set userid=%user%@%server%/%sid%
+set DATADIR=data
+set LOGFILE=%DATADIR%\%FILETAG%_import.log
+set DATFILE=%DATADIR%\data.dmp
+set USERID=%SYSUSER%/%SYSPASS%@%TNSID%
 
 rem 导入数据文件
-echo Import to %userid%
-imp PARFILE=params-import.txt USERID=%userid% LOG=%logfile% FILE=%datfile%
-echo Save log to %logfile%
-echo Import to %userid% >> %logfile%
+echo Import to %USERID%
+imp PARFILE=params-import.txt USERID=%USERID% LOG=%LOGFILE% FILE=%DATFILE%
+echo Save log to %LOGFILE%
+echo Import to %USERID% >> %LOGFILE%
