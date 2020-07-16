bc4cdf517c5b5c03f908522e1b5a3cbce25f1566
Jinghui Hu <hujinghui@buaa.edu.cn>
2020-07-16 10:44:26 +0800

    update

--- porter/import.sh
+++ porter/import.sh
@@ -22,7 +22,7 @@ LOGFILE="${DATADIR}/${FILETAG}_import.log"
 USERID="${SYSUSER}/${SYSPASS}@${TNSID}"
 
 # 导入数据文件
-echo "Import from $USERID"
+echo "Import into $USERID"
 imp PARFILE=params-import.txt USERID=$USERID LOG=$LOGFILE FILE=$DATFILE
 echo "Save log to $LOGFILE"
-echo "Import from $USERID" >> $LOGFILE
+echo "Import into $USERID" >> $LOGFILE
